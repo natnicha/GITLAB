cfe1b7f1d70712f082b4ad8186751e72472ae5f7
natnicha <nat.rodtong@gmail.com>
2015-04-20 14:33:31 +0700

    version 1

---
+++ Program #1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("Hello World for Program 1");
+<<<<<<< HEAD
+	printf("Change");
+=======
+	printf("Add me");
+>>>>>>> 5e2c5767b5fe57ce946fad4bdc26c843ae4719f1
+}
---
+++ Program #2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("Hello World for Program 2");
+}
